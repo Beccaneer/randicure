0e9bea370dd0795df723184ac6f75ac48bfba41d
Rebecca Thomson <thomson.rebecca@gmail.com>
2015-06-30 17:29:50 -0700

    little-changes

--- app/models/colour.rb
+++ app/models/colour.rb
@@ -1,5 +1,6 @@
 class Colour < ActiveRecord::Base
 	belongs_to :user
+	validates_length_of :brandname, :productname, :colourname, :maximum => 100 
 
 	def self.random_colours(number, userid)
 		colours = Colour.where(user_id: userid).sample(number)
--- app/models/tech.rb
+++ app/models/tech.rb
@@ -3,6 +3,7 @@ class Tech < ActiveRecord::Base
 	has_many :tutorials
 	validates :title, :mincol, :maxcol, :minaccents, :maxaccents, :presence => true
 	validates_numericality_of :maxcol, :greater_than_or_equal_to => :mincol
+	validates_numericality_of :maxcol, :less_than_or_equal_to => 20
 	validates_numericality_of :minaccents, :greater_than_or_equal_to => 0
 	validates_numericality_of :maxaccents, :minaccents, :less_than_or_equal_to => 10
 	validates_numericality_of :maxaccents, :greater_than_or_equal_to => :minaccents
