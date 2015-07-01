cb0718ac28ea2fe74a5f500f6e48ca6d8fbc1a04
Rebecca Thomson <thomson.rebecca@gmail.com>
2015-07-01 07:27:48 -0700

    second-little-changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,7 @@ class Ability
     #
        user ||= User.new # guest user (not logged in)
     	  if user.admin?
+					puts "User is admin"
         	can :manage, :all
 					can :access, :rails_admin   # grant access to rails_admin
 					can :dashboard              # grant access to the dashboard
