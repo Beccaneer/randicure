d4062f51bc8a6504a8c8f3f49c22ec522f9ac99c
Rebecca Thomson <thomson.rebecca@gmail.com>
2015-08-18 11:51:14 -0700

    updating from heroku

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ get 'welcome/index'
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
-  # Example of regular route:
+  # Example of regular route
   #   get 'products/:id' => 'catalog#view'
 
 	#get '/addfavmani', to: 'welcome#addfavmani'
